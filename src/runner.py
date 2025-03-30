b562ccbf5dc001b9dee561bf63d38ec2ffb36891
Harsh Gupta <gladiator098123@gmail.com>
2025-03-30 14:27:12 +0530

    adding api key

--- src/runner.py
+++ src/runner.py
@@ -2,13 +2,13 @@ from utils.api_key_rotate import APIKeyManager
 from utils.config import settings
 
 groq_api_manager = APIKeyManager(
-    api_keys=[settings.GROQ_API_KEY1, settings.GROQ_API_KEY2],
+    api_keys=[settings.GROQ_API_KEY1, settings.GROQ_API_KEY2, settings.GROQ_API_KEY3],
     rate_limit=30,
     cooldown_period=60
 )
 
 google_api_manager = APIKeyManager(
-    api_keys=[settings.GOOGLE_API_KEY1, settings.GOOGLE_API_KEY2],
+    api_keys=[settings.GOOGLE_API_KEY1, settings.GOOGLE_API_KEY2, settings.GOOGLE_API_KEY3, settings.GOOGLE_API_KEY4, settings.GOOGLE_API_KEY5, settings.GOOGLE_API_KEY6, settings.GOOGLE_API_KEY7],
     rate_limit=10,
     cooldown_period=60
 )
